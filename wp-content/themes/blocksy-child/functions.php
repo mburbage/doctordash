3aa3f0a6dcb0ec6dbe59c892ca8f16656541a7f1
mburbage <michael@redcircle.biz>
2023-03-28 14:18:14 -0400

    Font styles

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -3,5 +3,6 @@ if (! defined('WP_DEBUG')) {
 	die( 'Direct access forbidden.' );
 }
 add_action( 'wp_enqueue_scripts', function () {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	//wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_uri() );
 });
